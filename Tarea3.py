28ebe3288edc06675850f659a498eec74c5fb5a2
JuanSuarez <jp.juansuarez@gmail.com>
2022-09-23 21:12:12 -0300

    Added assignment 3.

---
+++ Tarea3.py
@@ -0,0 +1,6 @@
+pesoUsuario = input("Ingrese su peso: ")
+estaturaUsuario = input("Ingrese su estatura en metros: ")
+
+indiceMC =float(pesoUsuario) / (float(estaturaUsuario)**2)
+
+print("Su indice de masa corporal es de: " , indiceMC)
